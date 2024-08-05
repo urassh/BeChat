aca52b689d062d1f7964c1d9cd7fa393bcbbaaff
urassh <shuto.1011@icloud.com>
2024-08-05 13:43:47 +0900

    feat: firestorage系の実装を完了させた

--- BeChat/Firebase/MessageStore.swift
+++ BeChat/Firebase/MessageStore.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseStorage
 
 class MessageStore : MessageProtocol {
     private let db = Firestore.firestore()
+    private let storage = Storage.storage()
     private let COLLECTION = "messages"
+    private let STORAGE_URL = "gs://bechat-0811.appspot.com/image_message"
     
     func send(with message: any Message) {
         if message is TextMessage {
@@ -36,7 +39,7 @@ class MessageStore : MessageProtocol {
         return []
     }
     
-    func getTextMessage(id: String) async -> TextMessage? {
+    internal func getTextMessage(id: String) async -> TextMessage? {
         let docRef = db.collection(COLLECTION).document(id)
         do {
             return try await docRef.getDocument(as: TextMessage.self)
@@ -47,11 +50,32 @@ class MessageStore : MessageProtocol {
         return nil
     }
     
-    func getImageMessage(id: String) async -> ImageMessage? {
-        return nil
+    internal func getImageMessage(id: String) async -> ImageMessage? {
+        let ref = storage.reference(forURL: STORAGE_URL).child(id)
+        
+        guard let message: TextMessage = await getTextMessage(id: id) else { return nil }
+        
+        do {
+            let data = try await withUnsafeThrowingContinuation { (continuation: UnsafeContinuation<Data, Error>) in
+                ref.getData(maxSize: 100 * 1024 * 1024) { data, error in
+                    if let error = error {
+                        continuation.resume(throwing: error)
+                    } else if let data = data {
+                        continuation.resume(returning: data)
+                    }
+                }
+            }
+            
+            let image = UIImage(data: data)
+            
+            return ImageMessage(id: message.id, from_id: message.from_id, to_id: message.to_id, image: image)
+        } catch {
+            print("Error occurred! : \(error)")
+            return nil
+        }
     }
     
-    private func sendText(with message: TextMessage) {
+    internal func sendText(with message: TextMessage) {
         do {
             try db.collection(COLLECTION).document(message.id.uuidString).setData(from: message)
         } catch {
@@ -60,14 +84,25 @@ class MessageStore : MessageProtocol {
     }
     
     private func sendImage(with message: ImageMessage) {
+        guard let image = message.image else { return }
+        
         let textMessage: TextMessage = message.toText()
         
         do {
             try db.collection(COLLECTION).document(message.id.uuidString).setData(from: textMessage)
             
-            //TODO: ここにStorageに保存する処理を実装する。
+            let ref = storage.reference(forURL: STORAGE_URL).child(message.id.uuidString)
+            guard let jpegData = convertToJpegData(uiImage: image) else {
+                print("画像の変換に失敗しました")
+                return
+            }
+            ref.putData(jpegData as Data)
         } catch {
             print("Error Writing Document: \(error)")
         }
+    }
+    
+    private func convertToJpegData(uiImage: UIImage) -> NSData? {
+        return uiImage.jpegData(compressionQuality: 0.1) as? NSData
     }
 }
