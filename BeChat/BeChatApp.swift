753bfe9151359455dda3890d84f16a1c6c48716c
urassh <shuto.1011@icloud.com>
2024-08-05 11:42:17 +0900

    inial commit

---
+++ BeChat/BeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeChatApp.swift
+//  BeChat
+//
+//  Created by 浦山秀斗 on 2024/08/05.
+//
+
+import SwiftUI
+
+@main
+struct BeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeChat/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BeChat
+//
+//  Created by 浦山秀斗 on 2024/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
