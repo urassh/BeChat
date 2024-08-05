fbcd9e905321c7f95d206d5efab24a17ffd04c1c
saki0411 <chipdale411@gmail.com>
2024-08-06 03:11:23 +0900

    home-MessageView=UI

---
+++ BeChat/MessageListView/MessageView.swift
@@ -0,0 +1,45 @@
+//
+//  TodayMessageView.swift
+//  BeChat
+//
+//  Created by saki on 2024/08/05.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    @Binding var iconSize : CGFloat
+    var body: some View {
+        
+        VStack{
+            
+            VStack(alignment: .leading){
+                if iconSize >= 50.0{
+                    
+                    
+                    HStack{
+                        Image("sample2")
+                            .resizable()
+                            .clipShape(Circle())
+                            .frame(width: iconSize, height: iconSize)
+                            .padding(.trailing, 10)
+                        Text("saki")
+                            .minimumScaleFactor(0.1)
+                            .lineLimit(1)
+                        
+                    }
+                }
+                
+                Image("sample2")
+                    .resizable()
+                    .frame(maxWidth: 300, maxHeight: 500)
+                
+            }
+            
+        }
+        
+    }
+}
+#Preview {
+    MessageView(iconSize: .constant(50.0))
+}
---
+++ MessageListView.swift
@@ -0,0 +1,42 @@
+//
+//  MessageListView.swift
+//  BeChat
+//
+//  Created by saki on 2024/08/05.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    @State var messages = [String()]
+    var body: some View {
+        NavigationView{
+            VStack{
+                
+                MessageView(iconSize: .constant(50))
+                    .frame(width: 270,height: 480)
+                    .aspectRatio(contentMode: .fit)
+                    .navigationTitle("BeChat")
+                    .padding(10)
+                ScrollView (.horizontal) {
+                    HStack {
+                        ForEach(messages, id: \.self) { name in
+                            MessageView(iconSize: .constant(30.0))
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }
+                }
+                .padding(.horizontal,30)
+                
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    MessageListView()
+}
+
+
+
