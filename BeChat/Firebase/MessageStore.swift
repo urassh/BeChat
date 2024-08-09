823d04852d252a05eac5b22979eb50c05f2ac328
urassh <shuto.1011@icloud.com>
2024-08-10 00:52:15 +0900

    feat: home viewを実装してみた

--- BeChat/Firebase/MessageStore.swift
+++ BeChat/Firebase/MessageStore.swift
@@ -5,9 +5,9 @@
 //  Created by 浦山秀斗 on 2024/08/05.
 //
 
-import Foundation
 import FirebaseFirestore
 import FirebaseStorage
+import Foundation
 
 class MessageStore: MessageProtocol {
     private let db = Firestore.firestore()
@@ -15,103 +15,110 @@ class MessageStore: MessageProtocol {
     private let COLLECTION = "messages"
     private let STORAGE_URL = "gs://bechat-0811.appspot.com/image_message"
     private var listener: ListenerRegistration?
-    
+
     func send(with message: any Message) {
         if message is TextMessage {
             sendText(with: message as! TextMessage)
         }
-        
+
         if message is ImageMessage {
             sendImage(with: message as! ImageMessage)
         }
     }
-    
+
     func fetchAll(completion: @escaping (Result<[TextMessage], Error>) -> Void) {
         listener = db.collection(COLLECTION).addSnapshotListener { querySnapshot, error in
             if let error = error {
                 completion(.failure(error))
                 return
             }
-            
+
             guard let documents = querySnapshot?.documents else {
                 completion(.success([]))
                 return
             }
-            
+
             let messages = documents.compactMap { queryDocumentSnapshot in
                 try? queryDocumentSnapshot.data(as: TextMessage.self)
             }
             completion(.success(messages))
         }
     }
-    
+
     func getTextMessage(id: String) async -> TextMessage? {
         let docRef = db.collection(COLLECTION).document(id)
         do {
             return try await docRef.getDocument(as: TextMessage.self)
-        } catch {
+        }
+        catch {
             print("Error decoding city: \(error)")
         }
-        
+
         return nil
     }
-    
+
     func getImageMessage(id: String) async -> ImageMessage? {
         let ref = storage.reference(forURL: STORAGE_URL).child(id)
-        
+
         guard let message: TextMessage = await getTextMessage(id: id) else { return nil }
-        
+
         do {
-            let data = try await withUnsafeThrowingContinuation { (continuation: UnsafeContinuation<Data, Error>) in
+            let data = try await withUnsafeThrowingContinuation {
+                (continuation: UnsafeContinuation<Data, Error>) in
                 ref.getData(maxSize: 100 * 1024 * 1024) { data, error in
                     if let error = error {
                         continuation.resume(throwing: error)
-                    } else if let data = data {
+                    }
+                    else if let data = data {
                         continuation.resume(returning: data)
                     }
                 }
             }
-            
+
             let image = UIImage(data: data)
-            
-            return ImageMessage(id: message.id, from_id: message.from_id, to_id: message.to_id, image: image)
-        } catch {
+
+            return ImageMessage(
+                id: message.id, from_id: message.from_id, to_id: message.to_id, image: image)
+        }
+        catch {
             print("Error occurred! : \(error)")
             return nil
         }
     }
-    
+
     internal func sendText(with message: TextMessage) {
         do {
             try db.collection(COLLECTION).document(message.id.uuidString).setData(from: message)
-        } catch {
+        }
+        catch {
             print("Error Writing Document: \(error)")
         }
     }
-    
+
     private func sendImage(with message: ImageMessage) {
         guard let image = message.image else { return }
-        
+
         let textMessage: TextMessage = message.toText()
-        
+
         do {
             try db.collection(COLLECTION).document(message.id.uuidString).setData(from: textMessage)
-            
+
             let ref = storage.reference(forURL: STORAGE_URL).child(message.id.uuidString)
             guard let jpegData = convertToJpegData(uiImage: image) else {
                 print("画像の変換に失敗しました")
                 return
             }
             ref.putData(jpegData as Data)
-        } catch {
+        }
+        catch {
             print("Error Writing Document: \(error)")
         }
     }
-    
+
     private func convertToJpegData(uiImage: UIImage) -> NSData? {
         return uiImage.jpegData(compressionQuality: 0.1) as? NSData
     }
-    
+
     deinit {
         listener?.remove()
     }
