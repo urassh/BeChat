f737209d80c9e2c6414e32df9e6793b0304768eb
urassh <shuto.1011@icloud.com>
2024-08-09 23:45:07 +0900

    refactor: formated by swift format

--- BeChat/Firebase/AuthImpl.swift
+++ BeChat/Firebase/AuthImpl.swift
@@ -5,8 +5,8 @@
 //  Created by 浦山秀斗 on 2024/08/08.
 //
 
-import Foundation
 import FirebaseAuth
+import Foundation
 
 class AuthImpl: AuthProtocol {
     func login(user: UnAuthenticatedUser) async -> AuthenticatedUser {
@@ -15,19 +15,22 @@ class AuthImpl: AuthProtocol {
                 if let error = error {
                     print("Anonymous login failed: \(error.localizedDescription)")
                     continuation.resume(throwing: error as! Never)
-                } else if let authResult = authResult {
-                    let authenticatedUser = AuthenticatedUser(uid: authResult.user.uid, name: user.name)
+                }
+                else if let authResult = authResult {
+                    let authenticatedUser = AuthenticatedUser(
+                        uid: authResult.user.uid, name: user.name)
                     continuation.resume(returning: authenticatedUser)
                 }
             }
         }
     }
-    
+
     func logout() {
         do {
             try Auth.auth().signOut()
             print("User signed out successfully")
-        } catch let signOutError as NSError {
+        }
+        catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
     }
