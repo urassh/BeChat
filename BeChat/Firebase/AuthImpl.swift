b6eec48e1afbf871a1763a997635990a31fe2b25
saki0411 <chipdale411@gmail.com>
2024-08-10 17:48:28 +0900

    format

--- BeChat/Firebase/AuthImpl.swift
+++ BeChat/Firebase/AuthImpl.swift
@@ -7,23 +7,23 @@
 
 import FirebaseAuth
 
-
 class AuthImpl: AuthProtocol {
     func login(user: UnAuthenticatedUser) async throws -> AuthenticatedUser {
-           return try await withCheckedThrowingContinuation { continuation in
-               Auth.auth().signInAnonymously { authResult, error in
-                   if let error = error {
-                       print("Anonymous login failed: \(error.localizedDescription)")
-                       continuation.resume(throwing: error)
-                   } else if let authResult = authResult {
-                       let authenticatedUser = AuthenticatedUser(
-                           uid: authResult.user.uid, name: user.name)
-                       continuation.resume(returning: authenticatedUser)
-                   }
-               }
-           }
-       }
-    
+        return try await withCheckedThrowingContinuation { continuation in
+            Auth.auth().signInAnonymously { authResult, error in
+                if let error = error {
+                    print("Anonymous login failed: \(error.localizedDescription)")
+                    continuation.resume(throwing: error)
+                }
+                else if let authResult = authResult {
+                    let authenticatedUser = AuthenticatedUser(
+                        uid: authResult.user.uid, name: user.name)
+                    continuation.resume(returning: authenticatedUser)
+                }
+            }
+        }
+    }
+
     func logout() {
         do {
             try Auth.auth().signOut()
